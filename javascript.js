066885c4e934dcd43790273c891f0cd2cbaf6600
Kevin Leopoldino <kevinleopoldino@gmail.com>
2021-02-06 00:16:16 -0300

    d3-validacao de cadastro

--- javascript.js
+++ javascript.js
@@ -1,7 +1,19 @@
 
+$(document).ready(function(){
+	$('#cpf').mask('000.000.000-00').trigger('input')
+	$('#data').mask('00/00/0000').trigger('input')
+	$('#tel').mask('(00) 00000-0000').trigger('input')
+	$('#num').mask('0#')
+	 $('#cep').mask('00000-000');
+})
+
+
 function entrar() {
 	window.location.href='principal.html';
 }
 function sair(){
 	window.location.href='index.html';
+}
+function cadastro(){
+	window.location.href='cadastro.html';
 }
